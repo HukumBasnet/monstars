e3c951cc88a952fe5623fc0c24e823c2a005c188
reenahukum <big.b.it.traininig@gmail.com>
2020-01-27 18:32:37 +0545

    Adding serches and finishing the project

--- monsters/src/components/search-box/SearchBox.js
+++ monsters/src/components/search-box/SearchBox.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './search.css'
 
 const SearchBox = props => {
     return (
-        <div>
-            
-        </div>
+        <input
+        className = 'search-box'
+        type= 'search'
+        placeholder='search monsters'
+        onChange={ props.onSearchChange }
+        
+        
+        />
     )
 }
 
